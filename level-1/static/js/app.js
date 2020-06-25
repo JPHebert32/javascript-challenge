6841104a9b2d8a646f85bcad41750b4788f08508
JP Hebert <JPHebert32@gmail.com>
2020-06-24 22:34:41 -0600

    append to table

--- level-1/static/js/app.js
+++ level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
+  //console.log(tableData);
 
 // YOUR CODE HERE!
+var tbody = d3.select('tbody');
+
+data.forEach(function(ufoSighting) {
+  //console.log(ufoSighting);
+});
+
+
+
+ data.forEach((ufoSighting) => {
+  var row = tbody.append('tr');
+  Object.entries(ufoSighting).forEach(([key,value]) => {
+    var cell = row.append('td');
+    cell.text(value);
+  });
+ });
+
+ var form = d3.select('#datetime').property('value');
+  console.log(form);
