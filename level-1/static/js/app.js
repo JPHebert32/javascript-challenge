c2bbd20afb4158044a21d94d07a9e81c96424e5a
JP Hebert <JPHebert32@gmail.com>
2020-06-27 16:38:53 -0600

    filter by dateTime

--- level-1/static/js/app.js
+++ level-1/static/js/app.js
@@ -1,23 +1,34 @@
-// from data.js
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
-  //console.log(tableData);
 
-// YOUR CODE HERE!
+// Get a reference to the table body
 var tbody = d3.select('tbody');
 
-data.forEach(function(ufoSighting) {
-  //console.log(ufoSighting);
+function popTable(data) {
+  tbody.html('');
+  data.forEach((ufoSighting) => {
+    // Creating table rows for each row of ufroSighting data
+    var row = tbody.append('tr');
+    //Iterating thru each row for key and values
+    Object.entries(ufoSighting).forEach(([key,value]) => {
+      // Creating cells for posting table data
+      var cell = row.append('td');
+      cell.text(value);
 });
+});
+}
+
+// Populate the Table
+popTable(tableData);
 
 
+function handleClick() {
+  let dateValue  = d3.select('#datetime').property('value');
+  //Filter Data
+  let filteredData = tableData.filter(row => row.datetime === dateValue);
+  popTable(filteredData);
+  console.log(filteredData);
+}
 
- data.forEach((ufoSighting) => {
-  var row = tbody.append('tr');
-  Object.entries(ufoSighting).forEach(([key,value]) => {
-    var cell = row.append('td');
-    cell.text(value);
-  });
- });
-
- var form = d3.select('#datetime').property('value');
-  console.log(form);
+// Store value from form
+var submit = d3.select("#filter-btn").on("click", handleClick);
